0164cee7acb18849efb47c73f80bbb949a6d1ff8
Kristijan Burnik <kristijanburnik@gmail.com>
2013-12-09 23:20:45 +0100

    Fix the default path to phpQuery to be in the assumed project root directory

--- webcrawler.include.php
+++ webcrawler.include.php
@@ -1,6 +1,6 @@
 <?
 include_once( dirname(__FILE__).'/../project.php' );
 
-include_once( dirname(__FILE__).'/phpquery/phpQuery/phpQuery.php' );
+include_once( dirname(__FILE__).'/../phpquery/phpQuery/phpQuery.php' );
 
 ?>
